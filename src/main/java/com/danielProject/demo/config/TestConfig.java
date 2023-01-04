7c497311d8f09649a2b98162e916cb791041cf48
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2023-01-04 11:04:47 -0300

    Payment, One-to-one association

--- src/main/java/com/danielProject/demo/config/TestConfig.java
+++ src/main/java/com/danielProject/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.danielProject.demo.entities.Category;
 import com.danielProject.demo.entities.Order;
 import com.danielProject.demo.entities.OrderItem;
+import com.danielProject.demo.entities.Payment;
 import com.danielProject.demo.entities.Product;
 import com.danielProject.demo.entities.User;
 import com.danielProject.demo.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 		
 	}
 	
