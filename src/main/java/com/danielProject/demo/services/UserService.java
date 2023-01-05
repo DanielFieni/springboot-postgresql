da23edca007b95067eacbc1d03dbe08dba9e80ca
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2023-01-05 10:36:50 -0300

    Exception handling - findById

--- src/main/java/com/danielProject/demo/services/UserService.java
+++ src/main/java/com/danielProject/demo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.danielProject.demo.entities.User;
 import com.danielProject.demo.repositories.UserRepository;
+import com.danielProject.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	public User findById(Long id)
 	{
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
